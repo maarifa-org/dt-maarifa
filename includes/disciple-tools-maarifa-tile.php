f3c363baa0070c84c663dc3e4eb232e35cc2e766
Cairo Coder <cairocoder01@gmail.com>
2019-10-03 10:09:00 +0200

    Add maarifa data panel

--- includes/disciple-tools-maarifa-tile.php
+++ includes/disciple-tools-maarifa-tile.php
@@ -37,8 +37,103 @@ class Disciple_Tools_Maarifa_Tile
      */
     public function __construct()
     {
+        add_filter( "dt_custom_fields_settings", [ $this, "dt_contact_fields" ], 10, 2 );
+
+        add_filter( "dt_details_additional_section_ids", [ $this, "dt_maarifa_declare_section_id" ], 999, 2 );
+        add_action( "dt_details_additional_section", [ $this, "dt_maarifa_add_section" ] );
         add_filter( "dt_comments_additional_sections", [ $this, "add_comment_section" ], 10, 2 );
     } // End __construct()
+
+    public static function dt_contact_fields( array $fields, string $post_type = ""){
+        dt_write_log( __METHOD__ . ': start' );
+        //check if we are dealing with a contact
+        if ($post_type === "contacts"){
+            //check if the language field is already set
+            if ( !isset( $fields["maarifa_data"] )){
+                //define the language field
+                $fields["maarifa_data"] = [
+                    "name" => __( "Maarifa Data", "dt_maarifa" ),
+                    "type" => "array",
+                    "default" => []
+                ];
+            }
+        }
+        //don't forget to return the update fields array
+        return $fields;
+    }
+
+    public static function dt_maarifa_declare_section_id( $sections, $post_type = "" ) {
+        //check if we are on a contact
+        if ( $post_type === "contacts" ) {
+            $contact_fields = Disciple_Tools_Contact_Post_Type::instance()->get_custom_fields_settings();
+            //check if the language field is set
+            //check if content is there before adding empty tile
+            $contact_id    = get_the_ID();
+            if ( $contact_id ){
+                $contact       = Disciple_Tools_Contacts::get_contact( $contact_id, true, true );
+                if ( isset( $contact["maarifa_data"] ) ) {
+                    if ( isset( $contact_fields["maarifa_data"] ) ) {
+                        $sections[] = "contact_maarifa_data";
+                    }
+                }
+            }
+        }
+        return $sections;
+    }
+
+    public static function dt_maarifa_add_section( $section ) {
+        if ( $section == "contact_maarifa_data" ) {
+            $contact_id    = get_the_ID();
+            $contact       = Disciple_Tools_Contacts::get_contact( $contact_id, true, true );
+            $maarifa_data = [];
+            if ( isset( $contact["maarifa_data"] ) ) {
+                $maarifa_data = maybe_unserialize( $contact["maarifa_data"] );
+            }
+            ?>
+
+
+            <?php
+            if ( isset( $maarifa_data["id"] ) ) {
+                ?>
+                <label class="section-header">
+                    <?php esc_html_e( "Maarifa", "dt_maarifa" ) ?>
+                </label>
+
+                <div class="section-subheader">
+                    <?php esc_html_e( "ID", "dt_maarifa" ) ?>
+                </div>
+                <p><?php echo esc_html( $maarifa_data["id"] ) ?></p>
+                <?php
+            }
+            if ( isset( $maarifa_data["first_contact_details"] ) ) {
+                ?>
+                <div class="section-subheader">
+                    <?php esc_html_e( "First Contact from Campaign", "dt_maarifa" ) ?>
+                </div>
+                <p>
+                    <?php echo esc_html( $maarifa_data["first_contact_details"]["title"] ) ?>
+
+                    <?php if ( isset( $maarifa_data["first_contact_details"]["description"] ) ): ?>
+                        <br>
+                        <span class="campaign-description" style="font-style:italic">
+                            <?php echo esc_html( $maarifa_data["first_contact_details"]["description"] ) ?>
+                        </span>
+                    <?php endif; ?>
+
+                    <?php if ( isset( $maarifa_data["first_contact_details"]["dates"] ) ): ?>
+                        <br>
+                        <span class="campaign-dates-label" style="font-weight: 500">
+                            <?php echo esc_html( "Dates", "dt_maarifa" ) . ": " ?>
+                        </span>
+                        <span class="campaign-dates">
+                            <?php echo esc_html( $maarifa_data["first_contact_details"]["dates"] ) ?>
+                        </span>
+                    <?php endif; ?>
+                </p>
+                <?php
+            }
+        }
+    }
 
     public function add_comment_section( $sections, $post_type ) {
         if ( $post_type === "contacts" ) {
@@ -48,6 +143,11 @@ class Disciple_Tools_Maarifa_Tile
             ];
         }
         return $sections;
+    }
+
+    public function add_duplicate_check_field( $fields ) {
+        $fields[] = "maarifa_data";
+        return $fields;
     }
 }
 
