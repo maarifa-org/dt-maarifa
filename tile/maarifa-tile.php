086235af690cbc23bfc18bba338343330dca45b0
adventureit <adventure4it@gmail.com>
2025-12-04 17:06:25 +0200

    [DT-M3] Track digital responder in DT from RS
    Adding "Maarifa Responders" tags field on Maarifa tile.

--- tile/maarifa-tile.php
+++ tile/maarifa-tile.php
@@ -65,7 +65,16 @@ class Disciple_Tools_Maarifa_Tile
                     'hidden' => true
                 );
             }
+            if ( !isset( $fields['responders_name'] ) ){
+                $fields['responders_name'] = [ 
+                    'name' => 'Maarifa Responders', 
+                    'type' => 'tags', 
+                    "icon" => "/wp-content/themes/disciple-tools-theme/dt-assets/images/tag.svg",
+                    "readonly" => true, 
+                    'tile' => 'dt_maarifa'];    
+            }       
         }
+    
         //don't forget to return the update fields array
         return $fields;
     }
@@ -142,7 +151,9 @@ class Disciple_Tools_Maarifa_Tile
                 .ip-location-details dt { width: 30%; }
                 .ip-location-details dd { width: 70%; }
             </style>
+            <?php //if ( isset( $maarifa_data['responder_name'] ) ): ?>
 
+            <?php //endif; ?>
             <?php if ( isset( $maarifa_data['id'] ) ): ?>
 
                 <div class="section-subheader section-subheader-maarifa">
